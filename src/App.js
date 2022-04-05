460a59078b762a55790da4d07196a43356c93e50
Rachel de la Torre <Rachel.delatorre1@gmail.com>
2022-04-05 21:10:33 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import Home from './home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './nav';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar />
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" component={Home} />
+      </Routes>
+   </BrowserRouter>
     </div>
-  );
+  ); 
 }
 
 export default App;
---
+++ src/nav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function nav() {
+  return (
+    <div>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+        </ul>
+      </nav></div>
+  )
+}
